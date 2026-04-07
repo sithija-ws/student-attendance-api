4e1bff626f03c2e5b274464b55a786a98fca77ca
wolfyblue078 <wolfyblue078@gmail.com>
2026-04-07 21:10:16 +0530

    feat: setup detenv for advanced security 🔐

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import e from "express";
+import mongoose from "mongoose";
+require('dotenv').config();
 
 const app = e();
+
+//database
+
+const port = process.env.port || 3000;
 
 
 app.listen(3000, ()=>{
