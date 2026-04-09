251b7379e3a03c916f28283e23712a2947c37ba3
wolfyblue078 <wolfyblue078@gmail.com>
2026-04-10 02:40:45 +0530

    feat: implement mark attendance functionality with OTP validation

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -1,3 +1,4 @@
+import { compare } from "bcrypt";
 import Session from "../models/session.js";
 
 export const createSession = async (req, res) => {
@@ -47,6 +48,47 @@ export const createSession = async (req, res) => {
         console.log(error.message);
         res.status(500).json({
             message: "Backend Error", 
-        error: error.message});
+            error: error.message});
+    }
+}
+
+export const markAttendance = async (req,res) =>{
+    try {
+        const {otp} = req.body;
+        const studentID = req.user.id || req.user._id;
+        
+        const session = await Session.findOne({otp:otp});
+
+        if(!session){
+            return res.status(404).json({message: "Invalid OTP! ❌"});
+        }
+
+        if(new Date() > session.otpExpire){
+            return res.status(400).json({message: "OTP Expired! ❌"});
+        }
+
+        //compare session.location with student's req.body.location;
+        const updatedSession = await Session.findOneAndUpdate(
+            { _id: session._id, "attendees.student": { $ne: studentId } },
+            { 
+                $push: { 
+                    attendees: { student: studentId, timestamp: new Date() } 
+                } 
+            },
+            { new: true }
+        );
+
+        if (!updatedSession) {
+            return res.status(400).json({ message: "Attendance already marked! ✅" });
+        }
+
+
+        res.status(200).json({ message: "Attendance marked successfully! 🎉" });
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({
+            message: "Server error during attendance", 
+            error: error.message});
     }
 }
