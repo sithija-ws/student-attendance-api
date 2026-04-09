2803ab42e891aaecf3d6592f9b67f5062df89c08
wolfyblue078 <wolfyblue078@gmail.com>
2026-04-10 03:03:52 +0530

    feat: add distance calculation helper and integrate mark attendance route

--- routes/sessionRouter.js
+++ routes/sessionRouter.js
@@ -1,10 +1,11 @@
 import e from "express";
-import { createSession } from "../controllers/sessionController.js";
+import { createSession, markAttendance } from "../controllers/sessionController.js";
 import { verifyToken } from "../controllers/verifyMiddleware.js";
 
 const sessionRouter = e.Router();
 
 sessionRouter.post("/create",verifyToken ,createSession);
+sessionRouter.post("/mark-attendance", verifyToken, markAttendance);
 
 
 export default sessionRouter;
