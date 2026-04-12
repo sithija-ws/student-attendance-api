8935c3ecfd2bdeb12d5071e2a1fd66780f4a8c2a
wolfyblue078 <wolfyblue078@gmail.com>
2026-04-12 20:24:24 +0530

    feat: add generate token option for lecturer change password 🖥️🔐

--- controllers/lecturerController.js
+++ controllers/lecturerController.js
@@ -49,3 +49,57 @@ export const lecturerRegister = async (req,res)=>{
         })
     }
 }
+
+export const generateOTPL = async (req,res)=>{
+    try {
+        const {email} = req.body;
+        const lecturer = await Lecturer.findOne({email});
+        if(!lecturer){
+            return res.status(404).json({
+                message: "lecturer not found!"
+            })
+        }
+
+        //generate 6 digit OTP
+        const otp = Math.floor(100000 + Math.random() * 900000).toString();
+        lecturer.otp = otp;
+        lecturer.otpExpire = Date.now() + 5 * 60 * 1000;
+        await lecturer.save();
+
+
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: "sithija.ws@gmail.com",
+                pass: process.env.pswd,
+            },
+            });
+
+        await transporter.sendMail({
+            from: "sithija.ws@gmail.com",
+            to: email,
+            subject: "OTP for Password Reset",
+            text: `Your OTP is ${otp}`
+        });
+
+        res.status(200).json({ message: "OTP sent to email" });
+
+    } catch (error) {
+        console.log("OTP ERROR:", error.message);
+        res.status(500).json({
+            message: "internal server error "
+        });
+    }
+}
+
+
+export const changePassword = async (req,res)=>{
+    const {email, newPassword, confirmPassword} = req.body;
+
+    //check password confirmation
+    if(newPassword !== confirmPassword){
+        return res.status(400).json({
+            message: "passwords do not match!"
+        })
+    }
+}
--- routes/lecturerRouter.js
+++ routes/lecturerRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { lecturerRegister } from "../controllers/lecturerController.js";
+import { generateOTPL, lecturerRegister } from "../controllers/lecturerController.js";
 
 const lecturerRouter = express.Router();
 
 lecturerRouter.post("/register", lecturerRegister);
+lecturerRouter.post("/generate-otp", generateOTPL);
 
 export default lecturerRouter;
