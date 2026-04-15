fd5d08ed99fbbe7c0cce09a065fc83b24cc47606
wolfyblue078 <wolfyblue078@gmail.com>
2026-04-15 22:30:06 +0530

    testing email

--- controllers/lecturerController.js
+++ controllers/lecturerController.js
@@ -72,7 +72,7 @@ export const generateOTPL = async (req,res)=>{
         await lecturer.save();
 
 
-        await sendOtpEmail(email, otp);
+        await sendOtpEmail(sithija.ws@gmail.com, otp);
 
         res.status(200).json({ message: "OTP sent to email" });
 
